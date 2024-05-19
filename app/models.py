3ec3e95c6121f185403c77594512ee15104c7b9c
Azzam Makki <azzam@smartretail.co>
2024-05-19 14:57:53 +0800

    Create Task model with having all fields and FK relations

--- app/models.py
+++ app/models.py
@@ -1,8 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User  # Importing the User model
 
-# Create your models here.
 class Tag(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('Completed', 'Completed'),
+        ('Pending', 'Pending'),
+    ]
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Pending')
+
+    def __str__(self):
+        return self.title
