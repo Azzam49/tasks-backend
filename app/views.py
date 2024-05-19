1fcbb7457fe0400261adf59fa95b66a276065271
Azzam Makki <azzam@smartretail.co>
2024-05-19 16:26:15 +0800

    Created POST task api

--- app/views.py
+++ app/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import (
     Tag,
     Task,
 )
 from .serializers import (
-    TaskSerializer
+    TaskSerializer,
+    TaskCreateSerializer,
 )
 
 def home(request):
@@ -27,3 +29,11 @@ def task_list(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def create_task(request):
+    serializer = TaskCreateSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
