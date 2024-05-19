cb93ac1cd408257eb31638ec1c622aa26b01d284
Azzam Makki <azzam@smartretail.co>
2024-05-19 15:58:02 +0800

    Create GET tasks api and its serializer

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from app.views import (
     home,
     tag_list,
+    task_list,
 )
 
 urlpatterns = [
     path('', home, name='home'),
-    path('tags/', tag_list, name='tag-list'),
+    path('get/tags/', tag_list, name='tag-list'),
+    path('get/tasks/', task_list, name='task-list'),
 ]
