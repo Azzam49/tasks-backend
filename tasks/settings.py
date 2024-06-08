6f8f24b8c6685c7195c89867ab078a11e8c3918e
Azzam Makki <azzam@smartretail.co>
2024-06-08 18:31:50 +0800

    Remove media setting from settings.py and urls.py

--- tasks/settings.py
+++ tasks/settings.py
@@ -157,6 +157,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
